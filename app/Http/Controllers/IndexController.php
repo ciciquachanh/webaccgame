af2ef0371d939c887ee284421c55feae019851a1
Toan <toan@example.com>
2025-06-27 00:13:24 +0700

    Tạm bỏ kết nối DB trong IndexController để tránh lỗi

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,27 +1,52 @@
+<?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function home(){
-        $slider = []; // Tạm không lấy DB
+    public function home()
+    {
+        $slider = []; // Không lấy từ DB
         $category = [];
-        return view('pages.home', compact('category','slider'));
+        return view('pages.home', compact('category', 'slider'));
     }
 
-    public function dichvu(){
+    public function dichvu()
+    {
         return view('pages.services');
     }
 
-    public function acc($slug){
-        // Tạm bỏ xử lý DB
+    public function acc($slug)
+    {
         $category = null;
         $nicks = [];
-        return view('pages.acc', compact('slug','nicks','category'));
+        return view('pages.acc', compact('slug', 'nicks', 'category'));
     }
 
     public function detail_acc($slug, $ms)
     {
         $category = null;
-        $
+        $nick = null;
+        return view('pages.accgame', compact('nick', 'category'));
+    }
+
+    public function danhmuc_game($slug)
+    {
+        $category = null;
+        return view('pages.category', compact('category'));
+    }
+
+    public function danhmuccon($slug)
+    {
+        return view('pages.sub_category', compact('slug'));
+    }
+
+    public function detail_acc_simple($ms)
+    {
+        $nick = null;
+        $category = null;
+        return view('pages.accgame', compact('nick', 'category'));
+    }
+}
